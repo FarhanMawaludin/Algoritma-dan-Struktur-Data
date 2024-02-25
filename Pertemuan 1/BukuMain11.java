332ce036176aab775509fa1c4aafebf2c81c80c3
FarhanMawaludin <farhanmawaludin7@gmail.com>
2024-02-25 10:23:27 +0700

    pertemuan 2

--- Pertemuan 1/BukuMain11.java
+++ Pertemuan 1/BukuMain11.java
@@ -1,29 +1,3 @@
-
 public class BukuMain11 {
-    public static void main(String[] args) {
-        
-        Buku11 bk1 = new Buku11();
-
-        
-        bk1.judul = "Today Ends Tomorrow Comes";
-        bk1.pengarang = "Denanda Pratiwi";
-        bk1.halaman = 198;
-        bk1.stok =13;
-        bk1.harga = 71000;
-
-        
-        bk1.tampilInformasi();        
-        bk1.terjual(5);
-        bk1.gantiHarga(60000);
-        bk1.tampilInformasi();
-
-        Buku11 bk2 = new Buku11("Self Reward", "Maaheera Ayesha", 160, 29, 59000);
-        bk2.terjual(11);
-        bk2.tampilInformasi();
-
-        Buku11 NamaMahasiswa = new Buku11("manusia biasa", "Maman Supratman", 200, 50, 50000);
-        NamaMahasiswa.terjual(25);
-        NamaMahasiswa.tampilInformasi();
-        
-    }
+    
 }
